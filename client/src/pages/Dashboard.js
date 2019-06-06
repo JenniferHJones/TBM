ecb8fd9133cfefb08909836c128b12b8b605dce3
JJ <jenjones@ucdavis.edu>
2019-06-05 18:19:33 -0700

    fixed calendar

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { Redirect } from "react-router-dom";
 import { UserContext } from "../context";
 import Card from "../components/Cards/cards";
+import "./dashboard.css";
 
 function Dashboard(props) {
   const { state } = useContext(UserContext);
